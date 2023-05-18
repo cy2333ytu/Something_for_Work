282e83f563e969e8a73ee1d54b2439c7573b9abb
Yunzhe <Yunzhe.com>
2023-05-18 04:30:10 -0400

    2. appender - log.c

--- ccy/log.h
+++ ccy/log.h
@@ -1,9 +1,14 @@
-#ifndef CCY_LOH_H
-#define CCY_LOH_H
+#ifndef CCY_LOG_H
+#define CCY_LOG_H
 
 #include <string>
 #include <stdint.h>
 #include <memory>
+#include <list>
+#include <strstream>
+#include <fstream>
+#include <iostream>
+
 namespace ccy{
 
 // log event
@@ -40,9 +45,14 @@ public:
     typedef std::shared_ptr<LogAppender> ptr; 
     virtual ~LogAppender(){}
 
-    void log(LogLevel::Level level, LogEvent::ptr event);
-private:
+    virtual void log(LogLevel::Level level, LogEvent::ptr event) = 0;
+
+    void setFormatter(LogFormatter::ptr val) {m_formatter = val;}
+    LogFormatter::ptr  getFormatter() const {return m_formatter;}
+
+protected:
     LogLevel::Level m_level;
+    LogFormatter::ptr m_formatter;
 };
 
 // the format of log
@@ -60,20 +70,43 @@ public:
 
     Logger(const std::string& name = "root");
     void log(LogLevel::Level level, LogEvent::ptr event);
+
+    void debug(LogEvent::ptr event);
+    void info(LogEvent::ptr event);
+    void warn(LogEvent::ptr event);
+    void error(LogEvent::ptr event);
+    void fatal(LogEvent::ptr event);
+
+    void addAppender(LogAppender::ptr appender); 
+    void delAppender(LogAppender::ptr appender); 
+    LogLevel::Level getLevel() const {return m_level;}
+    void setLevel(LogLevel::Level val) {m_level = val;}
+
 private:
-    std::string m_name;
-    LogLevel::Level m_level;
-    LogAppender::ptr m_formatter;            
+    std::string m_name;                             // log name
+    LogLevel::Level m_level;                        // log level
+    std::list<LogAppender::ptr> m_appenders;        //  list of Appender
 };
 
 // output to console
 class ConsoleLogAppender: public LogAppender {
-
+public:
+    typedef std::shared_ptr<ConsoleLogAppender> ptr;
+    void log(LogLevel::Level level, LogEvent::ptr event) override;
 };
 
 // output to file
 class FileLogAppender: public LogAppender{
-
+public:
+    typedef std::shared_ptr<FileLogAppender> ptr;
+    FileLogAppender(const std::string& filename);
+    void log(LogLevel::Level level, LogEvent::ptr event) override;
+    
+// reopen file, if open successfully, return true
+    bool reopen();
+private:
+    std::string m_filename;
+    std::ofstream m_filestream;
 };
 
 }
